7a7684bc77fcedb8fbc6077d1705c7173d07e2dd
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-24 19:37:46 +0200

    ADD: adding client and counselor serializer classes

--- mental_health_support/counseling_app/serializers.py
+++ mental_health_support/counseling_app/serializers.py
@@ -19,3 +19,40 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = ["id", "user", "role", "bio", "profile_pictuer"]
 
 
+class ClientProfileSerializer(serializers.ModelSerializer):
+    """ """
+
+    profile = ProfileSerializer(read_only=True)
+    profile_id = serializers.PrimaryKeyRelatedField(
+        queryset=Profile.objects.all(),
+        source="profile",
+        write_only=True
+    )
+
+    class Meta:
+        model = ClientProfile
+        fields = ["id", "profile", "profile_id", "age", "gender", "preferences"]
+
+
+
+class CounselorApplicationSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)  # Show profile details, not editable
+    profile_id = serializers.PrimaryKeyRelatedField(
+        queryset=Profile.objects.all(),
+        source="profile",
+        write_only=True
+    )
+
+    class Meta:
+        model = CounselorApplication
+        fields = [
+            "id",
+            "profile",       # Read-only detailed profile
+            "profile_id",    # Write-only ID for linking
+            "status",
+            "specialization",
+            "experience_years",
+            "certifications",
+            "submitted_at"
+        ]
+        read_only_fields = ["status", "submitted_at"]
