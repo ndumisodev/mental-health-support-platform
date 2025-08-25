1fcddadd8716d212afc5670fdbb92bdd76794a43
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-25 19:40:09 +0200

    enabled admin for my models, registered my models on admin

--- mental_health_support/mental_health_support/urls.py
+++ mental_health_support/mental_health_support/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from counseling_app.views import hello_world
 
 urlpatterns = [
     path('admin/', admin.site.urls),
