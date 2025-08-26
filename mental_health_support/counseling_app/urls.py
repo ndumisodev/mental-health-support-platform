78f7bc830bcc2fb24f96813827928902a70ab197
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-26 21:48:17 +0200

    added rating model class for reviews

--- mental_health_support/counseling_app/urls.py
+++ mental_health_support/counseling_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProfileViewSet, ClientProfileViewSet, CounselorApplicationViewSet,SessionViewSet
+from .views import ProfileViewSet, ClientProfileViewSet, CounselorApplicationViewSet,SessionViewSet, ReviewViewSet
 
 router = DefaultRouter()
 router.register(r'profiles', ProfileViewSet, basename='profile')
 router.register(r'client-profiles', ClientProfileViewSet, basename='client-profile')
 router.register(r'counselor-applications', CounselorApplicationViewSet, basename='counselor-application')
 router.register(r'sessions', SessionViewSet, basename='session')
+router.register(r'reviews', ReviewViewSet, basename='review')
 
 urlpatterns = [
     path('', include(router.urls)),
