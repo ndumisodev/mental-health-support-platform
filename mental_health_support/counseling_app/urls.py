6a6bdee0c1a47347f527bebc27fa1d9b5fccde6c
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-26 20:19:44 +0200

    added docstrings for documentation for session serializer and for session viewset

--- mental_health_support/counseling_app/urls.py
+++ mental_health_support/counseling_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProfileViewSet, ClientProfileViewSet, CounselorApplicationViewSet
+from .views import ProfileViewSet, ClientProfileViewSet, CounselorApplicationViewSet,SessionViewSet
 
 router = DefaultRouter()
 router.register(r'profiles', ProfileViewSet, basename='profile')
 router.register(r'client-profiles', ClientProfileViewSet, basename='client-profile')
 router.register(r'counselor-applications', CounselorApplicationViewSet, basename='counselor-application')
+router.register(r'sessions', SessionViewSet, basename='session')
 
 urlpatterns = [
     path('', include(router.urls)),
